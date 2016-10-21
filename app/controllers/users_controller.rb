d70e77ee05911898dd92f6174051d790d8676be4
Kinasu <ashtoreet@gmail.com>
2016-10-21 14:36:27 +0300

    Views refactoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
 class UsersController < ApplicationController
   def index
+    @users = [
+      User.new(
+        id: 1,
+        name: 'Vadim',
+        username: 'installero',
+        avatar_url: 'https://secure.gravatar.com/avatar/71269686e0f757ddb4f73614f43ae445?s=100'
+      ),
+      User.new(
+        id: 2,
+        name: 'Misha',
+        username: 'aristofun'
+      )
+    ]
   end
 
   def new
@@ -12,6 +25,14 @@ class UsersController < ApplicationController
     @user = User.new(
       name: 'Valentina',
       username: 'ageratu',
-      avatar_url: 'https://avatarko.ru/img/avatar/10/zhivotnye_sobaka_9758.jpg')
+      avatar_url: 'https://avatarko.ru/img/avatar/10/zhivotnye_sobaka_9758.jpg'
+      )
+
+    @questions = [
+      Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2016')),
+      Question.new(text: 'В чем смысл жизни?', created_at: Date.parse('27.03.2016')),
+    ]
+
+    @new_question = Question.new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
-  resources :users
+  root 'users#index'
 
+  resources :users
+  resources :questions
 
   get 'show' => 'users#show'
 
